a79e0630e77bfe1e5715e8c2329b339398d3a775
Cel <celinenico22@gmail.com>
2021-06-03 21:12:53 -0400

    commit tree a1fb7c6f288925ac0e21dbbf089f640fe10ac485

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Your code here
+let wrapAdjective = function(sentence = "*") {
+    return function(adj = "special") {
+      return `You are ${sentence}${adj}${sentence}!`
+    }
+}
+
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+const Calculator = {
+    add: function(x, y) {
+        return (x + y)
+    },
+    subtract: function(a, b) {
+        return (a - b)
+    },
+    multiply: function(x, y) {
+        return (x * y)
+    },
+    divide: function(a, b) {
+        return (a / b)
+    }
+}
+
+let actionApplyer = function(start, ray) {
+    let a = start
+  
+    for (let i = 0; i < ray.length; i++ ){
+      a = ray[i](a)
+    }
+  
+    return a
+  }
